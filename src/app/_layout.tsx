5dae78831c8f3cc9f383bbda954c2816e30789f5
dancpluz <dan08jan@gmail.com>
2025-01-15 18:40:35 -0300

    feat: add fonts and assets

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,37 @@
 import "../global.css";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-  return <Slot />;
+  const [loaded, error] = useFonts({
+    'Inter-Black': require('../assets/fonts/Inter_18pt-Black.ttf'),
+    'Inter-Bold': require('../assets/fonts/Inter_18pt-Bold.ttf'),
+    'Inter-ExtraBold': require('../assets/fonts/Inter_18pt-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('../assets/fonts/Inter_18pt-ExtraLight.ttf'),
+    'Inter-Light': require('../assets/fonts/Inter_18pt-Light.ttf'),
+    'Inter-Medium': require('../assets/fonts/Inter_18pt-Medium.ttf'),
+    'Inter-Regular': require('../assets/fonts/Inter_18pt-Regular.ttf'),
+    'Inter-SemiBold': require('../assets/fonts/Inter_18pt-SemiBold.ttf'),
+    'Inter-Thin': require('../assets/fonts/Inter_18pt-Thin.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }}/>
+    </Stack>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,56 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          0: "#FFFFFF",
+          1: "#EBEBF5",
+          2: "#010618"
+        },
+        background: {
+          0: "#010618",
+          1: "#141927",
+          2: "#21283F",
+          3: "#EBEBF5",
+          4: "#FFFFFF"
+        },
+        border: {
+          0: "#FFFFFF",
+          1: "#EBEBF5",
+          2: "#21283F"
+        },
+        semantic: {
+          action: {
+            0: "#80B3FF",
+            1: "#C6DDFF"
+          },
+          success: {
+            0: "#4AD2C9",
+            1: "#CCEFEF"
+          },
+          warning: {
+            0: "#FFC374",
+            1: "#FFF0DD"
+          },
+          error: {
+            0: "#F66B6B",
+            1: "#F9CCCC"
+          },
+        }
+      },
+      fontFamily: {
+        iblack: ["Inter-Black", "sans-serif"],
+        ibold: ["Inter-Bold", "sans-serif"],
+        iextrabold: ["Inter-ExtraBold", "sans-serif"],
+        iextralight: ["Inter-ExtraLight", "sans-serif"],
+        ilight: ["Inter-Light", "sans-serif"],
+        imedium: ["Inter-Medium", "sans-serif"],
+        iregular: ["Inter-Regular", "sans-serif"],
+        isemibold: ["Inter-SemiBold", "sans-serif"],
+        ithin: ["Inter-Thin", "sans-serif"],
+      },
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
